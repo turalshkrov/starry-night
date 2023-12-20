57fd1803fdbaf5721a03b53b5e74e44f632dc40a
tural.shkrov <tural.shkrov@gmail.com>
2023-12-20 22:44:20 +0400

    update: card

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  return src('sass/**/*.scss')
+  return src('starry-night/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['sass/**/*.scss', '*.html'], buildStyles)
+  watch(['starry-night/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
