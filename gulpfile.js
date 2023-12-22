ec9dddb124a77b8f330e720cecdceae1c5ab1a61
tural.shkrov <tural.shkrov@gmail.com>
2023-12-22 13:24:36 +0400

    fix: file directory

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  return src('starry-night/**/*.scss')
+  return src('sass/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['starry-night/**/*.scss', '*.html'], buildStyles)
+  watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
