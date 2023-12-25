e7d94e91869b075e9c9f6417d866a07f5e914a79
tural.shkrov <tural.shkrov@gmail.com>
2023-12-25 12:35:32 +0400

    feat: utiliti classes with breakpoints

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,15 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
-const cssnano = require('gulp-cssnano');
 
 function buildStyles() {
-  return src('starry-night/**/*.scss')
-    .pipe(sass())
-    .pipe(purgecss({ content: ['*.html'] }))
-    .pipe(cssnano())
-    .pipe(dest('css'))
+  return src('src/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(dest('dist'))
 }
 
 function watchTask() {
-  watch(['starry-night/**/*.scss', '*.html'], buildStyles)
+  watch(['src/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask)
---
+++ index.js
@@ -0,0 +1,19 @@
+const starryNight = () => {
+  const navbarToggleButtons = document.querySelectorAll(".nav-toggle");
+  const navMenu = document.querySelectorAll(".nav-expand");
+  
+  navbarToggleButtons.forEach(button => {
+    const expandNavbarId = button.getAttribute("expand-target");
+    const expandNavbar = document.getElementById(expandNavbarId);
+  
+    button.addEventListener("click", () => {
+      expandNavbar.classList.toggle("nav-expand");
+    })
+  });
+  
+  navMenu.forEach(menu => {
+    menu.classList.remove("nav-expand");
+  });
+}
+
+export default starryNight;
